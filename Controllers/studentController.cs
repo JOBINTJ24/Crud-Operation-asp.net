cb31ee236d233f00d6951d531f03a3bd5dc13926
JOBINTJ24 <jobintj2023b@mca.ajce.in>
2023-03-26 23:54:05 +0530

    first commit

---
+++ Controllers/studentController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using Crud_Operation.Models;
+
+namespace Crud_Operation.Controllers
+{
+    public class studentController : Controller
+    {
+        Databse.db dblayer = new Databse.db();
+        // GET: student
+        public ActionResult Views()
+        {
+            string choice = "view";
+            DataSet ds = dblayer.Data_Views(choice);
+            ViewBag.student = ds.Tables[0];
+            return View();
+        }
+
+        public ActionResult add_record()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult add_record(FormCollection fc)
+        {
+            student st = new student();
+            st.Sname = fc["sname"];
+            st.Email = fc["email"];
+            st.Phone = fc["phone"];
+            st.department = fc["department"];
+            st.choice = fc["choice"];
+            dblayer.add_record(st);
+            TempData["msg"] = "Inserted";
+
+            return View();
+        }
+
+      
+
+        
+        public ActionResult delete_record1(int id)
+        {
+           string choice = "delete";
+            dblayer.delete_record(id,choice);
+            TempData["msg"] = "Deleted";
+            return RedirectToAction("Views");
+
+        }
+
+
+        public ActionResult Update_record1(int id)
+        {
+            String choice = "update";
+            DataSet ds = dblayer.Show_recod_byid(id,choice);
+            ViewBag.student = ds.Tables[0];
+            return View();
+        }
+
+
+
+
+        [HttpPost]
+        public ActionResult Update_record1(int id,FormCollection fc)
+        {
+            student student = new student();
+            student.id =id;
+            student.Sname = fc["sname"];
+            student.Email = fc["email"];
+            student.Phone = fc["phone"];
+            student.department = fc["department"];
+            student.choice = fc["choice"];
+            dblayer.update_record(student);
+            TempData["msg"] = "Updated";
+            return RedirectToAction("Views");
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Databse/db.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+using Crud_Operation.Models;
+using System.Drawing;
+
+namespace Crud_Operation.Databse
+{
+    public class db
+    {
+        SqlConnection sql = new SqlConnection("Data Source=LAPTOP-FVJ93DIM;Initial Catalog=employees;Integrated Security=True");
+         public void add_record(student student)
+        {
+            SqlCommand cmd = new SqlCommand("StudenCrud_MVC", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", student.id);
+            cmd.Parameters.AddWithValue("@sname", student.Sname);
+            cmd.Parameters.AddWithValue("@email", student.Email);
+            cmd.Parameters.AddWithValue("@phone", student.Phone);
+            cmd.Parameters.AddWithValue("@department", student.department);
+            cmd.Parameters.AddWithValue("@choice", student.choice);
+            sql.Open();
+            cmd.ExecuteNonQuery();
+            sql.Close();
+        }
+
+
+        public void update_record(student student)
+        {
+            SqlCommand cmd = new SqlCommand("StudenCrud_MVC", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", student.id);
+            cmd.Parameters.AddWithValue("@sname", student.Sname);
+            cmd.Parameters.AddWithValue("@email", student.Email);
+            cmd.Parameters.AddWithValue("@phone", student.Phone);
+            cmd.Parameters.AddWithValue("@department", student.department);
+            cmd.Parameters.AddWithValue("@choice", student.choice);
+            sql.Open();
+            cmd.ExecuteNonQuery();
+            sql.Close();
+        }
+
+        public DataSet Data_Views(string choice)
+        {
+            SqlCommand cmd = new SqlCommand("StudenCrud_MVC", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@choice", choice);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+
+        public void delete_record(int id, string choice)
+        {
+            SqlCommand cmd = new SqlCommand("StudenCrud_MVC", sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Parameters.AddWithValue("@choice", choice);
+            sql.Open();
+            cmd.ExecuteNonQuery();
+            sql.Close();
+        }
+
+
+        public DataSet Show_recod_byid(int id,string choice)
+        {
+            SqlCommand cmd = new SqlCommand("StudenCrud_MVC",sql);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@id",id);
+            cmd.Parameters.AddWithValue("@choice",choice);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+
+        }
+    }
+}
